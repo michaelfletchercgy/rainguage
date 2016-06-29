46653c11124905a8d61d4173e3d38bfb9f58b116
Michael Fletcher <m.fletcher@theplanet.ca>
2016-06-28 19:13:27 -0600

    Initial commit.

---
+++ src/bin/server.rs
@@ -0,0 +1,136 @@
+extern crate raingauge;
+extern crate libc;
+
+use std::fs::File;
+use std::fs::OpenOptions;
+
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
+
+use std::os::unix::io::AsRawFd;
+
+use std::path::Path;
+
+use std::thread::sleep;
+
+use std::time::Duration;
+
+use libc::epoll_event;
+use libc::epoll_create;
+use libc::epoll_ctl;
+use libc::epoll_wait;
+use libc::close;
+
+use libc::EPOLLPRI;
+use libc::EPOLLERR;
+use libc::EPOLL_CTL_ADD;
+
+struct Gpio {
+    epoll_fd: libc::c_int,
+    file: File
+}
+
+impl Gpio {
+    fn new(port: u32) -> Gpio {
+        let path_str = format!("/sys/class/gpio/gpio{}/value", port);
+        let path = Path::new(&path_str);
+
+        if !path.exists() {
+            let mut exp = OpenOptions::new().write(true).open("/sys/class/gpio/export").unwrap();
+            writeln!(exp, "{}", port).unwrap();
+            exp.flush().unwrap();
+        }
+
+        let edge_str = format!("/sys/class/gpio/gpio{}/edge", port);
+        let mut edge = OpenOptions::new().write(true).open(edge_str).unwrap(); 
+        write!(edge, "both").unwrap();
+        edge.flush().unwrap();
+
+        let file = File::open(path).unwrap();
+
+        unsafe {
+            let epollfd = epoll_create(1);
+
+            let mut event = epoll_event {
+                u64: 0,
+                events: EPOLLPRI as u32 | EPOLLERR as u32
+            };
+
+            let epoll_ctl_result = epoll_ctl(epollfd, EPOLL_CTL_ADD, file.as_raw_fd(), 
+                &mut event as *mut epoll_event);
+
+            if epoll_ctl_result != 0 {
+                panic!("epoll_ctl fail {}", epoll_ctl_result);
+            }
+
+            Gpio {
+                epoll_fd:epollfd,
+                file:file
+            }
+        }
+    }
+
+    fn next_value(&mut self) -> bool {
+        let mut events = Vec::<epoll_event>::with_capacity(1);
+        unsafe {
+            events.set_len(1);
+            let mut buffer = vec![0; 1];
+
+
+            println!("about to wait");
+            let num_ready = epoll_wait(self.epoll_fd, events.as_mut_ptr(), 5, -1);
+
+            if num_ready < 0 {
+                panic!("epoll_wait fail {}", num_ready);
+            }
+
+            self.file.read(&mut buffer).unwrap();
+            let result = buffer[0] == b'1';
+
+            self.file.seek(SeekFrom::Start(0)).unwrap();   
+            
+            result
+        }
+    }
+
+}
+
+impl Drop for Gpio {
+    fn drop(&mut self) {
+        unsafe {
+            close(self.epoll_fd);
+        }
+    }
+}
+
+struct NopOutput {
+    val:bool
+}
+
+impl raingauge::Output for NopOutput {
+    fn value(&mut self, val:bool) {
+        sleep(Duration::from_secs(2));
+        self.val = val;        
+    }
+}
+
+
+pub fn main() {
+    //let f = OpenOptions::new().write(true).open("/dev/ttyAMA0").unwrap();
+
+    let mut tipper_gpio = Gpio::new(24);
+    loop {
+        let value = tipper_gpio.next_value();
+        println!("value={}",value);
+    }
+    /*let tx_pwr = NopOutput { val:false };
+    let rg = raingauge::RainGauge::new(tx_pwr, f);
+
+    loop {
+        let value = tipper_gpio.next_value();
+        rg.tip(value);
+    }*/
+    //rg.stop();
+}
---
+++ src/lib.rs
@@ -0,0 +1,197 @@
+
+extern crate sqlite;
+
+use sqlite::Connection;
+use sqlite::Readable;
+use sqlite::State;
+
+use std::io::Write;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Sender;
+use std::thread;
+
+// an input
+// connects to a method with a value
+
+pub trait Output {
+    fn value(&mut self, new_value:bool);
+}
+
+pub struct RainGauge {
+    tx: Sender<Msg>
+}
+
+enum Msg {
+    Tipper(bool),
+    Stop
+}
+
+impl RainGauge {
+    pub fn new<P:'static+Output+Send,R: 'static+Write+Send>(mut transmit_power:P, mut radio_tx:R) -> RainGauge {
+        let (tx,rx) = channel();
+        
+        thread::spawn(move|| {
+            let mut tips = 0;
+            let mut last_value:Option<bool> = None;
+
+            let conn = Connection::open("data.sqlite").unwrap();
+
+            let mut stmt = conn.prepare("
+                    SELECT 1 
+                    FROM   sqlite_master 
+                    WHERE  type='table' and name='measurement'
+                ").unwrap();
+
+            let mut has_table = false;
+            while let State::Row = stmt.next().unwrap() {
+                has_table = true;
+            }
+
+            if !has_table {
+                conn.execute("CREATE TABLE measurement (
+                              id              INTEGER PRIMARY KEY,
+                              time_created    TEXT NOT NULL,
+                              value           I
+                              )").unwrap();
+            }
+
+            let mut sum_stmt = conn.prepare("SELECT SUM(value) FROM measurement").unwrap();
+            while let State::Row = sum_stmt.next().unwrap() {
+                tips = sum_stmt.read::<i64>(0).unwrap();
+            }
+
+
+            loop {
+                match rx.recv().unwrap() {
+                    Msg::Tipper(x) => {
+                        if last_value.is_some() {
+                            let last_val = last_value.unwrap();
+
+                            if x != last_val && x {
+                                tips = tips + 1;
+
+                                let dur = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap();
+                                let ts = dur.as_secs();
+                                let sql = format!("INSERT INTO measurement (time_created, value) VALUES ({}, 1)", ts);
+                                println!("sql={}", sql);
+                                conn.execute(sql).unwrap();
+                            }
+                        }
+
+                        last_value = Some(x);
+                    },
+                    Msg::Stop => {
+                        break;
+                    }
+                };
+
+                write!(radio_tx, "TIPS={}", tips).unwrap();
+                transmit_power.value(false);
+            }
+        });
+
+        RainGauge {
+            tx: tx
+        }
+    }
+
+    pub fn tip(&self, value:bool) {
+        self.tx.send(Msg::Tipper(value)).unwrap();
+    }
+
+    pub fn stop(&self) {
+        self.tx.send(Msg::Stop).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+    use Output;
+    use RainGauge;
+    use std::sync::mpsc::Sender;
+    use std::sync::mpsc::Receiver;
+    use std::sync::mpsc::channel;
+    use std::io::Error;
+
+    struct TestOutput {
+        val:bool
+    }
+
+    impl Output for TestOutput {
+        fn value(&mut self, val:bool) {
+            self.val = val;
+        }
+    }
+
+    struct TestWriter {
+        tx: Sender<u8>
+    }
+
+    impl Write for TestWriter {
+        fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+            
+            for x in buf {
+                self.tx.send(x.clone()).unwrap();
+            }
+
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> Result<(), Error> {
+            Ok(())
+        }
+    }
+
+    struct TestSink {
+        rx: Receiver<u8>
+    }
+
+    impl TestSink {
+        fn read_all(&mut self) -> Vec<u8> {
+            let mut res = Vec::new();
+
+            loop {
+                match self.rx.recv() {
+                    Ok(val) => {
+                        res.push(val);
+                    },
+                    Err(_) => { break; }    
+                }
+            }
+
+            res
+        }
+    }
+
+    fn test_channel() -> (TestWriter, TestSink) {
+        let (tx, rx) = channel();
+        
+        let writer = TestWriter {
+            tx: tx
+        };
+
+        let sink = TestSink {
+            rx: rx
+        };
+
+        (writer, sink)
+    }
+
+
+    #[test]
+    fn it_works() {
+        let tx_pwr = TestOutput { val:false };
+        let (writer, mut sink) = test_channel();
+        let rg = RainGauge::new(tx_pwr, writer);
+        rg.tip(false);
+        rg.tip(true);        
+        rg.tip(true);                
+        rg.stop();
+
+        use std::str;
+
+        let res = sink.read_all();
+        println!("{}", str::from_utf8(&res).unwrap());
+    }
+}
