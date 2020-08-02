6fd540ecd1d9d22137e09ed73ccc683927eb2a80
Michael Fletcher <m.fletcher@theplanet.ca>
2020-08-02 09:19:12 -0600

    Commiting the download firmware.

---
+++ downlink-firmware-c/receive_test.ino
@@ -0,0 +1,84 @@
+// Feather9x_RX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (receiver)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_TX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// for feather m0 RFM9x
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+void setup()
+{
+  digitalWrite(LED, HIGH);
+
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  delay(100);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  // the rf95 drive includes a simple header which we are not using in any way.  On the
+  // other side we have this set to four X's.
+  rf95.setPromiscuous(true);
+
+  digitalWrite(LED, LOW);
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      Serial.write(buf, len);
+      digitalWrite(LED, LOW);
+    }
+  }
+}
